e0968fe451b53b631db1ec4ae2c4eca7fab77663
Sujith M A <sujith.ma@beretail.in>
2025-05-17 12:54:36 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+ <div class="mx-auto h-screen max-w-screen-xl flex flex-col">
+  <div class="h-full">
+    <Header />
+  </div>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { FaHouseUser } from "vue-icons-plus/fa";
+import { GrContact } from "vue-icons-plus/gr";
+import { BsPersonWorkspace } from "vue-icons-plus/bs";
+import { ref } from 'vue'
+
+const activeLink = ref('about');
+const activeIconClass = 'scale-120';
+const hoverIconClass = 'group-hover:scale-120';
+const activeTextClass = 'translate-x-2';
+const hoverTextClass = 'group-hover:translate-x-2';
+const activeTextColor = 'text-slate-100';
+const inactiveTextColor = 'text-slate-600';
+
+</script>
+<template>
+    <header class="sticky top-0 flex flex-col w-[200px] h-full pt-[30%] px-12">
+        <nav>
+            <ul class="w-max flex flex-col gap-10">
+                <li class="group" :class="[activeLink === 'about' ? activeTextColor : inactiveTextColor]">
+                    <a href="#about" class="flex items-center gap-1" @click="activeLink = 'about'">
+                        <FaHouseUser :class="[activeLink === 'about' ? activeIconClass : hoverIconClass]"
+                            class="transition-all" />
+                        <span :class="[activeLink === 'about' ? activeTextClass : hoverTextClass]" class="transition-all">About</span>
+                    </a>
+                </li>
+                <li class="group" :class="[activeLink === 'experience' ? activeTextColor : inactiveTextColor]">
+                    <a href="#experience" class="flex items-center gap-1" @click="activeLink = 'experience'">
+                        <BsPersonWorkspace :class="[activeLink === 'experience' ? activeIconClass : hoverIconClass]" class="transition-all" />
+                        <span :class="[activeLink === 'experience' ? activeTextClass : hoverTextClass]" class="transition-all">Experience</span>
+                    </a>
+                </li>
+                <li  class="group" :class="[activeLink === 'contact' ? activeTextColor : inactiveTextColor]">
+                    <a href="#contact" class="flex items-center gap-1" @click="activeLink = 'contact'">
+                        <GrContact :class="[activeLink === 'contact' ? activeIconClass : hoverIconClass]" class="transition-all" />
+                        <span :class="[activeLink === 'contact' ? activeTextClass : hoverTextClass]" class="transition-all">Contact Me</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
