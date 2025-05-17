e0c1cf03250e5e899c4a76583ad6d6a7168d0b7f
Sujith M A <sujith.ma@beretail.in>
2025-05-18 03:50:20 +0530

    Style updated

--- src/components/About.vue
+++ src/components/About.vue
@@ -20,34 +20,34 @@ for (let i = 1; i < 6; i++) {
 
 </script>
 <template>
-    <section class="section pt-10 px-10 lg:px-0 gap-8 lg:h-screen lg:w-full">
+    <section class="section pt-20 px-10 lg:px-0 gap-8 lg:h-screen lg:w-full">
         <div class="h-full grid grid-cols-[1fr, 1fr] lg:grid-cols-[1fr_400px] lg:min-w-46 gap-5">
             <div class="flex justify-center lg:justify-start about-content">
 
                 <TransitionGroup name="fade" tag="div">
                     <div class="fade-in-items" v-for="item in arr" :key="item">
                         <h4 v-if="item === 1" class="text-emerald-600 text-xl text-center lg:text-left tracking-widest">
-                            Hi, my name is
+                            Hello, I am
                         </h4>
                         <h1 v-if="item === 2" class="text-8xl font-bold text-slate-400 mb-10 text-center lg:text-left">
-                            Sujith
+                            Sujith.
                         </h1>
                         <!-- <h2 v-if="item === 3" class="text-2xl font-normal text-slate-300 mb-10">Front-end Engineer</h2> -->
-                        <p v-if="item === 4" class="text-2xl mb-10 text-center lg:text-left">I am a passionate engineer
+                        <p v-if="item === 4" class="text-2xl mb-10 text-center lg:text-left">
+                            A passionate engineer
                             who
                             loves to build solid and
                             scalable front-end products.
-                            Currently I am Tech Lead at Expian Technologies Bengaluru
                         </p>
                         <div v-if="item === 5" class="flex justify-center lg:justify-start">
                             <ul class="ml-5 list-disc text-xl ">
-                                <li class="pb-2"><b class="text-emerald-400">10 years</b> of experience in front-end
+                                <li class="pb-2"><b class="text-emerald-500">10 years</b> of experience in front-end
                                     technologies</li>
                                 <li class="pb-2">Expert in <a target="_blank" href="https://angular.dev/"
-                                        class="link text-emerald-400 after:bg-emerald-400">Angular</a>; Built a ton of
+                                        class="link text-emerald-500 after:bg-emerald-500">Angular</a>; Built a ton of
                                     applications</li>
                                 <li class="pb-2">Gaining hands-on experience with <a target="_blank" href="https://vuejs.org/"
-                                        class="link text-emerald-400 after:bg-emerald-400">Vue.js</a>,
+                                        class="link text-emerald-500 after:bg-emerald-500">Vue.js</a>,
                                     <a href="https://react.dev/" target="_blank"
                                         class="link text-cyan-500 after:bg-cyan-500">React.js</a>
                                     and
@@ -61,7 +61,7 @@ for (let i = 1; i < 6; i++) {
                 </TransitionGroup>
 
             </div>
-            <div class="flex justify-center about-content md:!mt-[50px] sm:!mt-[50px]">
+            <div class="flex justify-center about-content pt-3">
                 <div class="w-[300px] h-[255px] lg:w-[400px] lg:h-[340px]">
                     <Transition name="slideIn">
                         <div v-if="showImage"
